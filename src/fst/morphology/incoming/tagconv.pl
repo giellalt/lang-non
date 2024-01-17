e681639cd8bb186696d3968009caa65aea474924
Flammie A Pirinen <flammie@iki.fi>
2024-01-17 13:32:23 +0100

    [Template merge] src/fst reorg

---
+++ src/fst/morphology/incoming/tagconv.pl
@@ -0,0 +1,83 @@
+#!/usr/bin/perl -w
+
+while (<>)
+
+{
+s/\+1\+sg/\+1Sg/g ;
+s/\+2\+sg/\+2Sg/g ;
+s/\+3\+sg/\+3Sg/g ;
+s/\+1\+du/\+1Du/g ;
+s/\+2\+du/\+2Du/g ;
+s/\+3\+du/\+3Du/g ;
+s/\+1\+pl/\+1Pl/g ;
+s/\+2\+pl/\+2Pl/g ;
+s/\+3\+pl/\+3Pl/g ;
+s/\+a-stem//g ;
+s/\+acc/\+Acc/g ;
+s/\+adj/\+A/g ;
+s/\+adv/\+Adv/g ;
+s/\+art/\+Art/g ;
+s/\+compar/\+Cmp/g ;
+s/\+comp/\+Cmp/g ;
+s/\+conj/+CC/g ;
+s/\+cons-stem//g ;
+s/\+dat/\+Dat/g ;
+s/\+def/\+Def/g ;
+s/\+dem/\+Dem/g ;
+s/\+det/\+Det/g ;
+s/\+du/\+Du/g ;
+s/\+fem/\+Fem/g ;
+s/\+gen/\+Gen/g ;
+s/\+i-stem//g ;
+s/\+interog/\+Interr/g ;
+s/\+imper/\+Imp/g ;
+s/\+imp/\+Imp/g ;
+s/\+indic/\+Ind/g ;
+s/\+inf/\+Inf/g ;
+s/\+interrog/\+Interr/g ;
+s/\+iptv/\+Imp/g ;
+s/\+ja-stem//g ;
+s/\+jo-stem//g ;
+s/\+masc/\+Msc/g ;
+s/\+middle/\+Middle/g ;
+s/\+nd-stem//g ;
+s/\+neut/\+Neu/g ;
+s/\+nom/\+Nom/g ;
+s/\+noun/\+N/g ;
+s/\+o-stem//g ;
+s/\+part/\+PrfPrc/g ;
+s/\+pl/\+Pl/g ;
+s/\+pp/\+PrfPrc/g ;
+s/\+prep/\+Pr/g ;
+s/\+pres_part/\+PrsPrc/g ;
+s/\+pres/\+Prs/g ;
+s/\+pretpres//g ;
+s/\+pret/\+Prt/g ;
+s/\+pron/\+Pron/g ;
+s/\+prop/\+Prop/g ;
+s/\+ptc/\+PrfPrc/g ;
+s/\+refl/\+Refl/g ;
+s/\+rel/\+Rel/g ;
+s/\+sg/\+Sg/g ;
+s/\+strong_III//g ;
+s/\+strong_II//g ;
+s/\+strong_IV//g ;
+s/\+strong_I//g ;
+s/\+strong_VII//g ;
+s/\+strong_V//g ;
+s/\+strong//g ;
+s/\+subjn/+CS/g ;
+s/\+subj/+Subj/g ;
+s/\+superl/+Superl/g ;
+s/\+u-stem//g ;
+s/\+verb/\+V/g ;
+s/\+wa-stem//g ;
+s/\+weak_3//g ;
+s/\+weak//g ;
+
+
+
+
+print ;
+
+}
